1a6b8e4cf112a00165ab83505a8cc6ff2d68f83c
Antal Árpád <arpad.antal@dyntell.com>
2025-05-12 17:24:29 +0300

    localization init

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App.tsx';
+import './i18n'; // 👈 import your i18n config
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
